e506b61113a7df0325fe2df75513a20549f82725
Florian <floheid@7fun.de>
2022-07-07 00:09:05 +0200

    clean solution

--- TacViewKillReader/Output.cs
+++ TacViewKillReader/Output.cs
@@ -1,10 +1,6 @@
 ﻿using ClosedXML.Excel;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TacViewKillReader
 {
